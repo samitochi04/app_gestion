68ab8f34e7b51f05b756de70aa13c2b0b8ef7f34
Samuel FOTSO <126283225+samitochi04@users.noreply.github.com>
2026-05-08 11:00:11 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.kairosinnovationtechnologies.online/erp'
+  apiUrl: 'https://api.kairosinnovationtechnologies.online/erp/api'
 };
